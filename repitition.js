5a1a1a42d4d44518212cb80a53ffb57e34d71b84
billy <billybilly@gmail.com>
2021-01-27 10:27:22 -0500

    Outputs words with given suffix

---
+++ repitition.js
@@ -0,0 +1,42 @@
+function findMatches() {
+	var dictionary = document.getElementById("dictionary").value;
+	if (!dictionary) {
+		alert("Please enter a dictionary");
+		return;
+	}
+	dict_words = dictionary.split('\n');
+	
+	var suffix = document.getElementById("suffix").value;
+	if (!suffix) {
+		alert("Please enter the suffix to search for");
+		return;
+	}
+	
+	var matches = document.getElementById("matches");
+	matches.value = "";
+	
+	for (var d in dict_words) {
+		if (dict_words[d].endsWith(suffix)) {
+			matches.value += dict_words[d] + '\n';
+		}
+	}
+}
+
+function createExercise() {
+	var suffix = document.getElementById("suffix").value;
+	var matches = document.getElementById("matches").value.split("\n");
+	
+	var exercise = document.getElementById("exercise");
+	exercise.value = "vvv vvv vvv\n";
+	for (var m in matches) {
+		for (var i = 0; i < 3; i++) {
+			exercise.value += suffix + "  ";
+		}
+		exercise.value += "\n";
+		for (var i = 0; i < 3; i++) {
+			exercise.value += matches[m] + "  ";
+		}
+		exercise.value += "\n";
+	}
+	
+}
